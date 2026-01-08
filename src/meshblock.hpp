75eaf21b099e7911c5e74e8b73c6c4f4f81aeb08
hwhitehead <hwhitehead25@gmail.com>
2026-01-08 16:48:12 +0000

    added mb funcs to device

--- src/meshblock.hpp
+++ src/meshblock.hpp
@@ -11,13 +11,13 @@ class MeshBlock {
         __host__ __device__ MeshBlock(vec3 xl, vec3 xr, std::vector<int> dims) {xl_ = xl; xr_ = xr; dims_ = dims; InitMeshBlock();}
 
         // routines
-        bool CalcIntercept(Ray r, float &tl, float &tr);
-        void CalcPath(Ray &r, float tl, float tr);
-        float PathSum(Ray &r);
-        int IntClamp(float f, float l, float r);
-        vec3 Edge(bool sign) {return (sign) ? xr_ : xl_;}
-        void ImportNumpyData(const std::string path);
-        int Size() {return dims_[0] * dims_[1] * dims_[2];}
+        __host__ __device__ bool CalcIntercept(Ray r, float &tl, float &tr);
+        __host__ __device__ void CalcPath(Ray &r, float tl, float tr);
+        __host__ __device__ float PathSum(Ray &r);
+        __host__ __device__ int IntClamp(float f, float l, float r);
+        __host__ __device__ vec3 Edge(bool sign) {return (sign) ? xr_ : xl_;}
+        __host__ __device__ void ImportNumpyData(const std::string path);
+        __host__ __device__ int Size() {return dims_[0] * dims_[1] * dims_[2];}
 
         // dtor
         ~MeshBlock();
@@ -36,7 +36,7 @@ MeshBlock::~MeshBlock() {
     cudaFree(data);
 }
 
-void MeshBlock::ImportNumpyData(const std::string path) {
+__host__ __device__ void MeshBlock::ImportNumpyData(const std::string path) {
     // import numpy 
     npy::npy_data d = npy::read_npy<float>(path);
     std::vector<float> npy_data = d.data;
@@ -52,11 +52,11 @@ void MeshBlock::ImportNumpyData(const std::string path) {
 
 }
 
-int MeshBlock::IntClamp(float f, float l, float r) {
+__host__ __device__ int MeshBlock::IntClamp(float f, float l, float r) {
     return std::max(l, std::min(std::floor(f), r));
 }
 
-void MeshBlock::InitMeshBlock() {
+__host__ __device__ void MeshBlock::InitMeshBlock() {
     dx_ = vec3();
     for (int i = 0; i <= 2; i++) {
         dx_[i] = (xr_[i] - xl_[i]) / dims_[i];
@@ -64,7 +64,7 @@ void MeshBlock::InitMeshBlock() {
     // initialise trace field
 }
 
-bool MeshBlock::CalcIntercept(Ray r, float &tl, float &tr) {
+__host__ __device__ bool MeshBlock::CalcIntercept(Ray r, float &tl, float &tr) {
     tl = 0.0, tr = 0.0;
     float tcmin, tcmax, tmin, tmax;
     for (int i = 0; i <= 2; i++) {
