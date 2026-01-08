b6026d87c3dc6d9db8990431bb4a14b1e7a64a7f
hwhitehead <hwhitehead25@gmail.com>
2026-01-08 13:44:09 +0000

    constructed simple hello world test

--- src/render.cu
+++ src/render.cu
@@ -12,11 +12,14 @@
 // custom local library imports 
 
 int main(void) {
+    // hello world test
+    std::cout << "Hello world." << std::endl;
     // perform basic test of libnpy functionality
     const std::string path {"simdata/data.npy"};
     npy::npy_data d = npy::read_npy<double>(path);
     std::vector<double> data = d.data;
     std::vector<unsigned long> shape = d.shape;
     std::cout << shape << std::endl;
+    std::cout << "finished init test." << std::endl;
     return 0;
 }
