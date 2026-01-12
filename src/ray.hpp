5c8f2581bad784ab7727de8e9e720bc6573bbca7
hwhitehead <hwhitehead25@gmail.com>
2026-01-12 16:27:16 +0000

    updated ray constructor

--- src/ray.hpp
+++ src/ray.hpp
@@ -15,7 +15,12 @@ class Ray {
                                 sign = vec3(inv_normal.x() < 0, 
                                             inv_normal.y() < 0,
                                             inv_normal.z() < 0);}
-        __device__ Ray(const vec3& o, const vec3& n) {origin = o; normal = n;}
+        __device__ Ray(const vec3& o, const vec3& n) {origin = o; 
+                                                        normal = n;
+                                                        inv_normal = vec3(1.0,1.0,1.0) / n;
+                                                        sign = vec3(inv_normal.x() < 0, 
+                                                                    inv_normal.y() < 0,
+                                                                    inv_normal.z() < 0);}
         
         // methods
         __device__ vec3 O() const {return origin;}
