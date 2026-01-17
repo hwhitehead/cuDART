7e6b535167add66d79f56cfb10b12394c123aefa
hwhitehead <hwhithead25@gmail.com>
2026-01-17 16:06:17 +0000

    testing small part

--- scripts/examples.py
+++ scripts/examples.py
@@ -7,7 +7,7 @@ import subprocess, os, sys, copy
 sys.path.append("..")
 from pysrc import *
 
-def demo_scene_gen(num_img = 10, verbose=True, remove_data=True):
+def demo_scene_gen(num_img = 1, verbose=True, remove_data=True):
 
     # define targets
     npy_load_str = os.path.join(host_dir, "inputs/sn_alt.npy")
@@ -34,7 +34,7 @@ def demo_scene_gen(num_img = 10, verbose=True, remove_data=True):
     scene = Scene(npy_load_str, npy_save_str, cameras)
 
     # render and save images
-    scene.render(verbose=verbose, max_mem=4)
+    scene.render(verbose=verbose, max_mem=1)
     scene.plot(png_save_str, verbose=verbose, remove_data=True)
 
 if __name__ == "__main__":
