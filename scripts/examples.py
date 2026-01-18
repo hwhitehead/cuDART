5beea12e80478872125621e6f32f737d47e81cc0
hwhitehead <hwhithead25@gmail.com>
2026-01-18 16:55:47 +0000

    path err

--- scripts/examples.py
+++ scripts/examples.py
@@ -24,7 +24,7 @@ def render_from_mesh(verbose=True, remove_data=True):
 
     data_dir = os.path.join(host_dir, "inputs/mesh_demo")
     npy_save_str = os.path.join(host_dir, "inputs/mesh_demo")
-    png_save_str = os.path.join(data_dir, "outputs/mesh_demo/img")
+    png_save_str = os.path.join(host_dir, "outputs/mesh_demo/img")
 
     # build camera
     camera = Camera()
