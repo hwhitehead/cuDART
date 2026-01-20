250e15e37bc47c27b37be28afb4d6890e7c06f57
hwhitehead <hwhitehead25@gmail.com>
2026-01-20 14:16:02 +0000

    index err

--- src/main.cu
+++ src/main.cu
@@ -388,7 +388,7 @@ int main(int argc, char *argv[]) {
     int mem_start = 0;
     std::cout << "num_mb = " << num_meshblocks << std::endl;
     std::cout << "d_bytes = " << d_bytes << std::endl;
-    std::cout << "first_size = " << all_mb_info[n].mb_size << std::endl;
+    std::cout << "first_size = " << all_mb_info[0].mb_size << std::endl;
 
     for (int n = 0; n < num_meshblocks; n++) {
         vec3 xl = all_mb_info[n].xl;
