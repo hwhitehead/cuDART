92d4f1ec51dad1ca71aab717726c434136a523cf
hwhitehead <hwhitehead25@gmail.com>
2026-01-22 16:55:35 +0000

    testing const duo cam

--- scripts/examples.py
+++ scripts/examples.py
@@ -43,7 +43,7 @@ def render_labelled_example():
 
     # build camera
     c2 = Camera()
-    theta = (10.0 / 180) * np.pi
+    theta = (75.0 / 180) * np.pi
     phi = (179.0 / 180) * np.pi
     tilt = (-38.0 / 180) * np.pi
     c2.set_sph_pos(r = 2.0, theta = theta, phi = phi, target_origin = True)
