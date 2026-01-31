f0a70783dba1dc1989b45095808d09758a62707f
hwhitehead <hwhithead25@gmail.com>
2026-01-31 19:49:27 +0200

    chasing err in meshblock init

--- src/main.cu
+++ src/main.cu
@@ -156,7 +156,7 @@ int main(int argc, char *argv[]) {
         all_mb_info = load_unlabelled_meshblock(input_str, h_all_data, h_bytes, relativistic, verbose);
     }
     int num_meshblocks = all_mb_info.size();
-    std::cout << "num_mb = " << num_meshblocks << std::endl;
+    std::cout << "mb_size = " << all_mb_info[0].mb_size << std::endl;
 
     // determine VRAM limitations
     float tolerance = 0.95; // use this fraction of available vram
