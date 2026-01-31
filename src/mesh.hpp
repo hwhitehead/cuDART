7cd49c95c51e8919f8c6219199148743127a0889
hwhitehead <hwhithead25@gmail.com>
2026-01-31 12:16:22 +0200

    more func ext

--- src/mesh.hpp
+++ src/mesh.hpp
@@ -14,7 +14,7 @@ class Mesh {
         __device__ Mesh(MeshBlock **l, int num_mb) {mb_list = l, num_meshblocks = num_mb;}
         
         // routines
-        __device__ float calc_trace(const Ray &r);
+        __device__ float calc_trace(const Ray &r, bool relativistic);
 
         // members
         MeshBlock **mb_list;
