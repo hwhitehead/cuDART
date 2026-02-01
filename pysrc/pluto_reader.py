5ab0becf417ea7a304657097dd01cfc7a99146af
hwhitehead <hwhithead25@gmail.com>
2026-02-01 23:30:48 +0200

    testing render with scraped data

--- pysrc/pluto_reader.py
+++ pysrc/pluto_reader.py
@@ -256,8 +256,6 @@ class PlutoReader:
                     emm_local = emm_local * self.units.undersampling_factor * self.volume_factor
                     emissivities.append(emm_local)
 
-                print(np.shape(P.x1))
-                print(np.shape(emissivities))
                 if i == 0:
                     particles = pd.DataFrame(np.array([P.x1, P.x2, P.x3]+emissivities).T,
                                                 columns=["x1", "x2", "x3"] + ["emm_freq_" + frequency[2:] for frequency in self.frequencies], dtype=pd.Float64Dtype())
--- scripts/examples.py
+++ scripts/examples.py
@@ -147,7 +147,7 @@ def render_jet(relativistic=False):
     print("cuDART: starting jet render example...")
 
     # define targets
-    npy_load_str = "/mnt/kocsis1/cuDART_wdir/snapshot0490_mirror_relativistic_10.npy"
+    npy_load_str = "/mnt/kocsis1/cuDART_wdir/emm_data.npy"
     npy_save_str = "/mnt/kocsis1/cuDART_wdir/raw"
     png_save_str = "/mnt/kocsis1/cuDART_wdir/img"
 
@@ -185,4 +185,4 @@ def render_jet(relativistic=False):
 
 if __name__ == "__main__":
 
-    render_jet(True)
+    render_jet(False)
