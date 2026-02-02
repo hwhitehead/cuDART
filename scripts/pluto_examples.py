01429dbdba9a7adb2756931078a4c4a0089bb48b
hwhitehead <hwhithead25@gmail.com>
2026-02-02 14:24:04 +0200

    testing full pipe no boost

--- scripts/pluto_examples.py
+++ scripts/pluto_examples.py
@@ -6,7 +6,7 @@ import numpy as np
 sys.path.append("..")
 from pysrc import *
 
-def run_pluto_convert_example():
+def extract_pluto_data_example():
 
     config_file = "/mnt/kocsis1/cuDART_wdir/jet_analyst/config.ini"
     load_dir = "/mnt/kocsis1/cuDART_wdir/jet_data/"
@@ -29,18 +29,48 @@ def run_pluto_convert_example():
                         apply_boost = apply_boost,
                         blur_kwargs = blur_kwargs)
 
-    load_str = os.path.join(emm_dir, "emm_" + frequencies[0] + ".npy")
-    boosted_data = np.load(load_str)
-    max_vx1 = np.max(boosted_data[...,1])
-    max_vx2 = np.max(boosted_data[...,2])
-    max_vx3 = np.max(boosted_data[...,3])
-    min_vx1 = np.min(boosted_data[...,1])
-    min_vx2 = np.min(boosted_data[...,2])
-    min_vx3 = np.min(boosted_data[...,3])
-    print(min_vx1, max_vx1)
-    print(min_vx2, max_vx2)
-    print(min_vx3, max_vx3)
+def render_pluto_data_example(relativistic=False):
+
+    print("cuDART: starting jet render example...")
+
+    # define targets
+    npy_load_str = "/mnt/kocsis1/cuDART_wdir/emm_data/emm_1000MHz.npy"
+    npy_save_str = "/mnt/kocsis1/cuDART_wdir/emm_img/raw"
+    png_save_str = "/mnt/kocsis1/cuDART_wdir/emm_img/img"
+
+    # build template camera
+    template_camera = Camera()
+    template_camera.num_pixels_X = 1024
+    template_camera.num_pixels_Y = 1024
+    template_camera.tilt = (60.0 / 180) * np.pi
+    template_camera.length_X = 0.66
+    template_camera.length_Y = 0.66
+
+    # build camera array, inherit from template
+    num_img = 100
+    phi = epsilon
+    theta_ar = np.linspace(epsilon,np.pi - epsilon,num_img, endpoint=False)
+    cameras = []
+    for theta in theta_ar:
+        camera = copy.deepcopy(template_camera)
+        camera.set_sph_pos(r = 2.0, theta = theta, phi = phi, target_origin = True)
+        cameras.append(camera)
+    print("initialised cameras")
+
+    # generate scene
+    scene = Scene(npy_load_str, npy_save_str, cameras)
+    print("built scene")
+
+    # render and save images
+    scene.render(verbose = True, relativistic = relativistic)
+    print("finished rendering raw images")
+
+    scene.plot_wlightcurve(png_save_str, cmap = "afmhot", verbose = True, remove_raw_images = True, vmin=18, vmax=21)
+    print("finished rendering rasterised images")
+
+    print("unlablled render example finished.")
 
 if __name__ == "__main__":
 
-    run_pluto_convert_example()
+    extract_pluto_data_example()
+    render_pluto_data_example(False)
