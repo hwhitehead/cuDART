2229ed3675d62025462b9e6d76135dea895f11ba
hwhitehead <hwhitehead25@gmail.com>
2026-02-16 12:05:55 +0000

    moved nvprof to render, duh

--- scripts/pluto_examples.py
+++ scripts/pluto_examples.py
@@ -128,10 +128,10 @@ def render_pluto_data_example(relativistic=False, remove_raw_images = True, prof
     print("built scene")
 
     # render and save images
-    scene.render(verbose = True, relativistic = relativistic)
+    scene.render(verbose = True, relativistic = relativistic, profile = profile)
     print("finished rendering raw images")
 
-    scene.plot(png_save_str, cmap = "afmhot", verbose = True, profile = profile, remove_raw_images = remove_raw_images, vmin=18, vmax=21)
+    scene.plot(png_save_str, cmap = "afmhot", verbose = True, remove_raw_images = remove_raw_images, vmin=18, vmax=21)
     print("finished rendering rasterised images")
 
     print("unlablled render example finished.")
