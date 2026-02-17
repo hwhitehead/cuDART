da95cda607ef5df2f6e61d1cad3f0507304bc30b
hwhitehead <hwhitehead25@gmail.com>
2026-02-17 14:37:36 +0000

    full suite test

--- scripts/pluto_examples.py
+++ scripts/pluto_examples.py
@@ -463,20 +463,17 @@ def extract_profiler_times():
 
     fig.savefig("/scratch/github/cuDART_wdir/profile.png", dpi=300, bbox_inches="tight")
 
-
-
-
 if __name__ == "__main__":
 
     data_dir = "/mnt/kocsis1/cuDART_wdir/profiling/data"
     prof_dir = "/mnt/kocsis1/cuDART_wdir/profiling/profiles"
     timing_str = "/mnt/kocsis1/cuDART_wdir/profiling/timings.npy"
     timing_png = "/mnt/kocsis1/cuDART_wdir/profiling/timings.png"
-    D_span = [64,128,256]
-    N_span = [64,128,256]
+    D_span = [64,128,256,512]
+    N_span = [64,128,256,512]
     profiler = Profiler(data_dir = data_dir, prof_dir = prof_dir)
-    # profiler.build(D_span = D_span, save_boosted = True)
-    #profiler.run(N_span = N_span, D_span = D_span, num_iter=10)
-    #profiler.save_timings(save_str = timing_str, N_span = N_span, D_span = D_span)
+    profiler.build(D_span = D_span, save_boosted = True)
+    profiler.run(N_span = N_span, D_span = D_span, num_iter=10)
+    profiler.save_timings(save_str = timing_str, N_span = N_span, D_span = D_span)
     profiler.plot_timings(load_str = timing_str, save_str = timing_png, N_span = N_span, D_span = D_span)
     
