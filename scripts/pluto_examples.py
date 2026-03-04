0efbb31a28433cb549dbe773eff0bf9cce6103ac
hwhitehead <hwhitehead25@gmail.com>
2026-03-04 15:39:41 +0000

    stash lightcurve

--- scripts/pluto_examples.py
+++ scripts/pluto_examples.py
@@ -118,7 +118,8 @@ def render_pluto_data_example(relativistic=False, remove_raw_images = True, save
     print("built scene")
 
     # render and save images
-    scene.render(verbose = True, relativistic = relativistic, save_profile = save_profile, doppler_index = doppler_index)
+    scene.calc_lightcurve("/mnt/kocsis1/cuDART_wdir/emm_img/index3.6/lum.npy")
+    #scene.render(verbose = True, relativistic = relativistic, save_profile = save_profile, doppler_index = doppler_index)
     print("finished rendering raw images")
 
     #scene.plot(png_save_str, cmap = "afmhot", verbose = True, remove_raw_images = remove_raw_images, vmin=18, vmax=21)
