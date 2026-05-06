86d7af4ec2451beb9f0bd8894d59051beb57d26c
hwhitehead <hwhitehead25@gmail.com>
2026-05-06 13:34:28 +0100

    removed erroneous comma

--- src/camera.hpp
+++ src/camera.hpp
@@ -68,7 +68,7 @@ __host__ std::vector<Camera> load_cameras(char *camera_char, bool verbose) {
             if (!(iss >> inp0 >> inp1 >> inp2 >> inp3 >> inp4 >> inp5 >> inp6 >> inp7 >> inp8 >> inp9 >> inp10 >> inp11 >> inp12)) {
                 std::stringstream err_msg;
                 err_msg << "### FATAL ERROR in main ###\n";
-                err_msg << "Unable to parse line " << line_count << "of camera file at " << camera_str << std::endl;
+                err_msg << "Unable to parse line " << line_count << " of camera file at " << camera_str << std::endl;
                 CUDART_ERROR(err_msg);
             } else {
                 // read line by line
