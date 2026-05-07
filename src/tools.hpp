ed67a23159889420a77331ea297e4b66b0e8c29c
hwhitehead <hwhitehead25@gmail.com>
2026-05-07 13:27:38 +0100

    lookback refactor

--- src/tools.hpp
+++ src/tools.hpp
@@ -69,18 +69,14 @@ __device__ float calc_boost_factor(vec3 beta_vec, vec3 view_vec, float doppler_i
 __device__ float calc_lookback_factor(float s, TraceArgs trace_args) {
     // caculate the lerp weighting factor between temporal states
     float t_bar = trace_args.t_obs - s * trace_args.inv_c; // lookback time at distance s along line-of-sight
-    int m_bar = floor(t_bar * trace_args.inv_snapshot_dt);
-    if (m_bar > trace_args.num_snapshots ) { // temporal overshoot
-        if (trace_args.snapshot_index != trace_args.num_snapshots - 1) { // only last snap contributes
-            return 0; 
-        }
-    } else if (trace_args.snapshot_index < m_bar) {
-        return 0;
-    } else if (trace_args.snapshot_index > m_bar + 1) {
-        return 0;
+    int m_bar = floor(t_bar * trace_args.inv_snapshot_dt); // leading snapshot s.t. t_bar \in [m_bar, m_bar+1]
+    if (trace_args.snapshot_index >= m_bar && trace_args.snapshot_index <= m_bar + 1) {
+        float lerp_factor = abs(t_bar - trace_args.snapshot_index * trace_args.snapshot_dt) * trace_args.inv_snapshot_dt;
+        return 1 - lerp_factor; // snapshot is afjacent, lerp contribution
+    } else if (m_bar > trace_args.num_snapshots && trace_args.snapshot_index == trace_args.num_snapshots - 1) {
+        return 1; // snapshot is last snapshot for late observer, full contribtuon
     } 
-    float lerp_factor = abs(t_bar - trace_args.snapshot_index / trace_args.inv_snapshot_dt) * trace_args.inv_snapshot_dt;
-    return 1 - lerp_factor;
+    return 0; // snapshot is not adjacent, no contribution
 }
 
 #endif
