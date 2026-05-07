56e3f4de5b0f8c289dea7234c63811f08e3a41d2
hwhitehead <hwhitehead25@gmail.com>
2026-05-07 13:14:33 +0100

    bug tracker

--- src/meshblock.hpp
+++ src/meshblock.hpp
@@ -262,7 +262,7 @@ __device__ float MeshBlock::calc_trace(const Ray &r, TraceArgs trace_args) {
             if (trace_args.lookback) {
                 trace_weight *= calc_lookback_factor(t_current, trace_args);
             }
-            if (trace_weight != 0) {
+            if (trace_weight != 0) { // ALWAYS ZERO
                 printf("%.3f\n", trace_weight);
             }
             trace += trace_weight * all_data[data_index];    
