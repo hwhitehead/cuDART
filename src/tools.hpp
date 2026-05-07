8a695493961d6c9198caa785bebacaa321ec3f42
hwhitehead <hwhitehead25@gmail.com>
2026-05-07 16:11:55 +0100

    catch early observers

--- src/tools.hpp
+++ src/tools.hpp
@@ -69,6 +69,16 @@ __device__ float calc_boost_factor(vec3 beta_vec, vec3 view_vec, float doppler_i
 __device__ float calc_lookback_factor(float s, TraceArgs trace_args) {
     // caculate the lerp weighting factor between temporal states
     float t_bar = trace_args.t_obs - s * trace_args.inv_c; // lookback time at distance s along line-of-sight
+    
+    // handle early observer (lookback time preceedes simulation data)
+    if (t_bar < 0) { 
+        if (trace_args.snapshot_index == 0) {
+            return 1; // snapshot is first snapshot for early observer, full contribution
+        } else {
+            return 0; // no contribution 
+        }
+    }
+
     int m_bar = floor(t_bar * trace_args.inv_snapshot_dt); // leading snapshot s.t. t_bar \in [m_bar, m_bar+1]
     if (trace_args.snapshot_index >= m_bar && trace_args.snapshot_index <= m_bar + 1) {
         float lerp_factor = abs(t_bar - trace_args.snapshot_index * trace_args.snapshot_dt) * trace_args.inv_snapshot_dt;
