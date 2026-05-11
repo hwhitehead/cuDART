84101a175c14abfafab67356ec56f86fabd822ca
hwhitehead <hwhithead25@gmail.com>
2026-05-11 18:27:13 +0100

    offset init

--- scripts/pluto_examples.py
+++ scripts/pluto_examples.py
@@ -418,6 +418,7 @@ def render_lookback_example(relativistic=False, remove_raw_images = True, save_p
     T_in_Myr = 0.5 * L_in_kpc / v_in_kpc_per_Myr # duration to reach domain edge
     dist_to_camera_in_kpc = 2 * L_in_kpc
     t_delay_in_Myr = dist_to_camera_in_kpc * kpc_to_m / (c_light * Myr_to_s)
+    t_delay_in_Myr *= 0.75
     for t in np.linspace(t_delay_in_Myr, t_delay_in_Myr + T_in_Myr, num_img):
         camera = copy.deepcopy(template_camera)
         camera.t_obs = t
@@ -456,6 +457,6 @@ def render_lookback_example(relativistic=False, remove_raw_images = True, save_p
 
 if __name__ == "__main__":
 
-    build_blob_data(num_snapshots=20)
+    #build_blob_data(num_snapshots=20)
     #render_pluto_data_example(relativistic=False, remove_raw_images = False, append=False)
     render_lookback_example(relativistic=True, remove_raw_images = False)
