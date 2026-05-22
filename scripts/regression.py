50d2f40cdbb34f43dfd452f0905a478f82a05e94
hwhitehead <hwhitehead25@gmail.com>
2026-05-22 14:15:26 +0100

    caught err in time offset

--- scripts/regression.py
+++ scripts/regression.py
@@ -194,10 +194,9 @@ def run_lookback_test(load_dir, save_dir, sim_args, camera_args, verbose=True):
 
     # calculate stop time (when receding ejectum reaches maximal extent)
     x_max_in_m = 0.5 * sim_args["L_domain"] * np.sin(theta) * kpc_to_m                          # max obs blob displacement for given theta
-    d_in_m = x_max_in_m * (1 - v_in_c * np.cos(theta)) / (v_in_c * np.sin(theta)) + D_in_m      # invert superluminal motion eq to calc flight time
-    dt_max_in_s = d_in_m / c_light                                                              # observer time when RECEDING blob reaches domain edge
-    dt_max = dt_max_in_s / Myr_to_s                                                             # cast to astro/code units    
-    t_max = t_min + dt_max                                                                      # offset by t_min
+    d_in_m = x_max_in_m * (1 + v_in_c * np.cos(theta)) / (v_in_c * np.sin(theta)) + D_in_m      # invert superluminal motion eq to calc flight time
+    t_max_in_s = d_in_m / c_light                                                               # observer time when RECEDING blob reaches domain edge
+    t_max = t_max_in_s / Myr_to_s                                                               # cast to astro/code units    
 
     # generate array of cameras, evenly seperated in observer time
     if (verbose): print(r"building {0} cameras evenly spanning observer time in [{1},{2}]".format(camera_args["num_img"],t_min, t_max))
