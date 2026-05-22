0c9ec8e37986c702b6ce08065a673b3216d04563
hwhitehead <hwhitehead25@gmail.com>
2026-05-22 14:39:00 +0100

    added missing cast to kpc

--- scripts/regression.py
+++ scripts/regression.py
@@ -52,10 +52,9 @@ def build_regression_suite(save_dir, sim_args, verbose=True, target_theta=None):
     # determine cadence
     if target_theta is not None:
         crit_fac = (1 - v_in_c * np.cos(target_theta)) / np.sin(target_theta)
-        dt_crit_in_s = crit_fac * sim_args["r_blob"] / (v_in_c * c_light)
+        dt_crit_in_s = crit_fac * sim_args["r_blob"] * kpc_to_m / (v_in_c * c_light)
         dt_crit = dt_crit_in_s / Myr_to_s
         num_snapshots_crit = int(T_in_Myr / dt_crit)
-        print(num_snapshots_crit)
         num_snapshots = num_snapshots_crit if (num_snapshots_crit > sim_args["num_snapshots"]) else sim_args["num_snapshots"]
     else:
         num_snapshots = sim_args["num_snapshots"]    
