171dd810c7108546a72002b46dae2ee29bf0f325
hwhitehead <hwhitehead25@gmail.com>
2026-06-01 14:01:14 +0100

    calc bounds in code units

--- src/main.cu
+++ src/main.cu
@@ -303,18 +303,28 @@ int main(int argc, char *argv[]) {
         int m_lower = 0, m_upper = num_snapshots - 1; // if no flexload, use full time range
         if (flexload) { // TODO: this can also be performed more simply in code units
             float domain_r_max = 0.5 * std::sqrt(3.0); // TODO: load this as part of header (via mesh_xr, mesh_xl)
-            float d_min_in_kpc = (camera_r_min - domain_r_max) * L_domain;          // minimum camera-domain seperation
-            float d_max_in_kpc = (camera_r_max - domain_r_max) * L_domain;          // maixmum camera-domain seperation
-            std::cout << "d_min_in_kpc" << d_min_in_kpc << ", d_max_in_kpc = " << d_max_in_kpc << std::endl;
-            float t_min_in_Myr = camera_t_min - d_max_in_kpc / c_in_kpc_per_Myr;    // earliest contributing snapshot time
-            float t_max_in_Myr = camera_t_max - d_min_in_kpc / c_in_kpc_per_Myr;    // latest contributing snapshot time
-            std::cout << "t_min_in_Myr" << t_min_in_Myr << ", t_max_in_Myr = " << t_max_in_Myr << std::endl;
-            int m_min = std::floor(t_min_in_Myr * trace_args.inv_snapshot_dt);          // earliest contributing snapshot index
-            int m_max = std::ceil(t_max_in_Myr * trace_args.inv_snapshot_dt);           // latest contributing snapshot index 
-            std::cout << "m_min = " << m_min << ", m_max = " << m_max << std::endl;
+            float d_min = camera_r_min - domain_r_max;
+            float d_max = camera_r_max - domain_r_max;
+            float t_min = camera_t_min - d_max * trace_args.inv_c;
+            float t_max = camera_t_max - d_min * trace_args.inv_c;
+            int m_min = std::floor(t_min * trace_args.inv_snapshot_dt);          // earliest contributing snapshot index
+            int m_max = std::ceil(t_max * trace_args.inv_snapshot_dt);           // latest contributing snapshot index 
             m_lower = (m_min > m_lower) ? m_min : m_lower;                          // start loop at earliest
             m_upper = (m_max < m_upper) ? m_max : m_upper;                          // end loop at latest
             std::cout << "m_lower = " << m_lower << ", m_upper = " << m_upper << std::endl;
+
+            // float d_min_in_kpc = (camera_r_min - domain_r_max) * L_domain;          // minimum camera-domain seperation
+            // float d_max_in_kpc = (camera_r_max - domain_r_max) * L_domain;          // maixmum camera-domain seperation
+            // std::cout << "d_min_in_kpc" << d_min_in_kpc << ", d_max_in_kpc = " << d_max_in_kpc << std::endl;
+            // float t_min_in_Myr = camera_t_min - d_max_in_kpc / c_in_kpc_per_Myr;    // earliest contributing snapshot time
+            // float t_max_in_Myr = camera_t_max - d_min_in_kpc / c_in_kpc_per_Myr;    // latest contributing snapshot time
+            // std::cout << "t_min_in_Myr" << t_min_in_Myr << ", t_max_in_Myr = " << t_max_in_Myr << std::endl;
+            // int m_min = std::floor(t_min_in_Myr * trace_args.inv_snapshot_dt);          // earliest contributing snapshot index
+            // int m_max = std::ceil(t_max_in_Myr * trace_args.inv_snapshot_dt);           // latest contributing snapshot index 
+            // std::cout << "m_min = " << m_min << ", m_max = " << m_max << std::endl;
+            // m_lower = (m_min > m_lower) ? m_min : m_lower;                          // start loop at earliest
+            // m_upper = (m_max < m_upper) ? m_max : m_upper;                          // end loop at latest
+            // std::cout << "m_lower = " << m_lower << ", m_upper = " << m_upper << std::endl;
         }
 
         // allocate data on host
