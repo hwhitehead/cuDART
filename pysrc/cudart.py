9b5daf5488fc09cbf5c1c5875bf4be6b2971b231
hwhitehead <hwhitehead25@gmail.com>
2026-06-11 14:21:52 +0100

    updated formatting for nsys write

--- pysrc/cudart.py
+++ pysrc/cudart.py
@@ -364,7 +364,7 @@ class Scene:
         command = [path_to_executable, "-i", self.load_str, "-s", self.save_dir,"-c",self.temp_camera_file]
         # run executable with nvprof
         if save_profile is not None: 
-            command = ["nsys", "profile", "--stats=true","--log-file",save_profile] + command
+            command = ["nsys", "profile", "--stats=true","--export=text","--output={0}".format(save_profile)] + command
         # pass verbose flag 
         if verbose_cpp: 
             command = command + ["-v"]
