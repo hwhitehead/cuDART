1bc152703eec9daf643da4071ebd5e381a0c80b8
hwhitehead <hwhitehead25@gmail.com>
2026-06-12 11:24:44 +0100

    added fractional duration report

--- pysrc/cudart.py
+++ pysrc/cudart.py
@@ -525,26 +525,29 @@ class Profiler:
         for task in gpu_csv_labels:
             row = np.where(gpu_df["Operation"] == task)[0][0]         
             num_calls = int(gpu_df["Instances"].iloc[row])
-            duration = float(gpu_df["Total Time (ns)"].iloc[row]) * 1e-9
-            average = float(gpu_df["Avg (ns)"].iloc[row]) * 1e-6
-            gpu_duration_sum += duration
-            print("{0}: {1} calls in {2:.3f}s (average {3:.3f}ms)".format(task, num_calls, duration, average_ms))
+            duration_s = float(gpu_df["Total Time (ns)"].iloc[row]) * 1e-9
+            average_ms = float(gpu_df["Avg (ns)"].iloc[row]) * 1e-6
+            gpu_duration_sum += duration_s
+            print("{0}: {1} calls in {2:.3f}s (average {3:.3f}ms)".format(task, num_calls, duration_s, average_ms))
         total_gpu_duration = gpu_df["Total Time (ns)"].sum()
-        print("GPU (other): {0:.3f}s".format(total_gpu_duration - gpu_duration_sum))
+        print("GPU (other) = {0:.3f}s".format(total_gpu_duration - gpu_duration_sum))
         print("Total GPU Duration = {0:.3f}s".format(total_gpu_duration))
+        print("Fractional GPU Duration = {0:.3f}%".format(100.0 * total_gpu_duration / self.wallclock_duration))
         print("\n")
         print("OSRT Summary:")
         osrt_duration_sum = 0
         for task in osrt_csv_labels:
             row = np.where(ostr_df["Name"] == task)[0][0]         
             num_calls = int(ostr_df["Num Calls"].iloc[row])
-            duration = float(ostr_df["Total Time (ns)"].iloc[row]) * 1e-9
-            average = float(gpu_df["Avg (ns)"].iloc[row]) * 1e-6
-            osrt_duration_sum += duration
-            print("{0}: {1} calls in {2:.3f}s (average {3:.3f})".format(task, num_calls, duration, average_ms))
-        print("Total OSRT Duration = {0:.3f}s".format(osrt_duraiton_sum))
-        print("OSRT (other): {0:.3f}s".format(self.wallclock_duration - osrt_duration_sum - total_gpu_duration))
-
+            duration_s = float(ostr_df["Total Time (ns)"].iloc[row]) * 1e-9
+            average_ms = float(gpu_df["Avg (ns)"].iloc[row]) * 1e-6
+            osrt_duration_sum += duration_s
+            print("{0}: {1} calls in {2:.3f}s (average {3:.3f})".format(task, num_calls, duration_s, average_ms))
+        osrt_total = self.wallclock_duration - total_gpu_duration
+        osrt_other = osrt_total - osrt_duration_sum 
+        print("OSRT (other) = {0:.3f}s".format(osrt_other))
+        print("Total OSRT Duration = {0:.3f}s".format(osrt_total))
+        print("Fractional OSRT Duration = {0:.3f}%".format(100.0 * osrt_total / self.wallclock_duration))
 
         if verbose:
             print("GPU CSV")
