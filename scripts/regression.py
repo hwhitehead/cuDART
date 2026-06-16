45edbc98633e5e41753671e1978264f23c22e7e6
hwhitehead <hwhitehead25@gmail.com>
2026-06-16 14:41:36 +0100

    render and save jet

--- scripts/regression.py
+++ scripts/regression.py
@@ -351,8 +351,8 @@ def run_lookback_test(load_dir, save_dir, sim_args, camera_args, verbose = True,
 
     # render and save images
     save_profile = os.path.join(save_dir, "profiling.txt")
-    # scene.render(verbose = verbose, relativistic = camera_args["relativistic"], lookback = True, verbose_cpp = verbose, flexload = flexload,
-    #             save_profile = save_profile)
+    scene.render(verbose = verbose, relativistic = camera_args["relativistic"], lookback = True, verbose_cpp = verbose, flexload = flexload,
+                save_profile = save_profile)
     if (verbose): print("finished rendering raw images.")
 
     if (camera_args["save_fig"]):
