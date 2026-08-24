0d478b419e5b40a3f6d0ac92c28877973e37ff14
hwhitehead <hwhiteead25@gmail.com>
2026-08-24 11:14:56 +0200

    id'd other deprecated reference

--- scripts/regression.py
+++ scripts/regression.py
@@ -134,7 +134,7 @@ def build_labelled_regression_suite(save_dir, sim_args, verbose=True, sphere_in_
     # define simulation parameters
     v_in_c = np.sqrt(1 - 1.0 / sim_args["Gamma"] ** 2)                          # calculate ejecta velocity
     v_in_kpc_per_Myr = v_in_c * c_light / (kpc_to_m / Myr_to_s)                 # cast to astro units
-    r_blob_in_code = sim_args["r_blob"] / sim_args["L_domain"]                  # cast to code units (where L_domain = 1.0)
+    r_blob_in_code = sim_args["r_in_kpc"] / sim_args["L_domain"]                  # cast to code units (where L_domain = 1.0)
     T_in_Myr = 0.5 * sim_args["L_domain"] / v_in_kpc_per_Myr                    # calc duration for blob to reach domain edge
     
     # build empty domain 
