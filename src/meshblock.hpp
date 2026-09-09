5e16d52e923cf15649c36db41c215583fce30b1e
hwhitehead <hwhiteead25@gmail.com>
2026-09-09 22:31:03 +0200

    debug cleanup

--- src/meshblock.hpp
+++ src/meshblock.hpp
@@ -169,10 +169,6 @@ __host__ std::vector<MeshBlockInfo> load_labelled_meshblocks(std::string input_s
         }
 
         std::vector<unsigned long int> data_shape = npy_to_host(input_str, h_all_data, h_bytes, verbose, false);
-
-        std::cout << "break post npy_to_host parse" << std::endl;
-        return all_mb_info;
-
         bool beta_in_data = (data_shape.size() > 3);
         all_mb_info[n].beta_in_data = beta_in_data;
         all_mb_info[n].mem_start = mem_offset;
